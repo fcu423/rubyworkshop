7870d41526c0cc45b573ab652cd0b27525f7ca08
Felipe Correa <fcu423@gmail.com>
2016-08-02 20:04:42 -0500

    - Added bootstrap styles - New and Create actions implemented for posts

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,9 +1,24 @@
 class PostsController < ApplicationController
     def index
         @posts = Post.all
+
+        if params[:created] == "created"
+            @message = "New post created successfully! =]"
+        end
     end
 
     def new
-        
+        @post = Post.new
     end
+
+    def create
+        post = Post.new(post_params)
+        post.save
+        redirect_to action: "index", created: "created"
+    end
+
+    private 
+        def post_params
+            params.require(:post).permit(:title, :body)
+        end
 end
